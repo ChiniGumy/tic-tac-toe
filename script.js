2b44dcb1868e5b2a2b62f065ff24e3b7f4fcf5ff
ChiniGumy <dsebastiansr@hotmail.com>
2022-11-29 19:10:36 -0500

    [ADD] UI features, Winner title

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let flag = 0;
 let board = [ , , , , , , , , ];
 const buttons = document.querySelectorAll('.game-button');
+const winnerTitle = document.getElementById('winner-title');
 
 function drawFigure(event){
 	
 	const button = event.target;
 	
 	if (button.innerHTML !== ''){
-		alert('Ya esta asignado');
+		winnerTitle.innerText = '¡La casilla ya está llena!';
+		winnerTitle.style.color='var(--red-error)';
 		return;
 	}
 	
 	if ((flag % 2) === 0){
-		button.innerHTML = 'X';
 		board[button.id] = 'X';
+		button.innerHTML = 'X';
+		button.style.color = 'var(--red)';
 	} else {
-		button.innerHTML = 'O';
 		board[button.id] = 'O';
+		button.innerHTML = 'O';
+		button.style.color = 'var(--blue)';
 	}
 	flag++;
 };
@@ -33,61 +37,64 @@ function determineWinner(){
 	// Rows
 	
 	if (board[0] === 'X' & board[1] === 'X' & board[2] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 		
 	} else if (board[3] === 'X' & board[4] === 'X' & board[5] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 
 	} else if (board[6] === 'X' & board[7] === 'X' & board[8] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 
 	} else if (board[0] === 'O' & board[1] === 'O' & board[2] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 
 	} else if (board[3] === 'O' & board[4] === 'O' & board[5] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 
 	} else if (board[6] === 'O' & board[7] === 'O' & board[8] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 
 	} else if (board[0] === 'X' & board[3] === 'X' & board[6] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 		
 	} else if (board[1] === 'X' & board[4] === 'X' & board[7] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 
 	} else if (board[2] === 'X' & board[5] === 'X' & board[8] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 
 	} else if (board[0] === 'O' & board[3] === 'O' & board[6] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 
 	} else if (board[1] === 'O' & board[4] === 'O' & board[7] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 		
 	} else if (board[2] === 'O' & board[5] === 'O' & board[8] === 'O'){
-		alert('O GANO');
+		winnerTitle.innerText = '¡Jugador O gana!!';
 		disableButtons();
 
 	} else if (board[0] === 'X' & board[4] === 'X' & board[8] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
 		
 	} else if (board[2] === 'X' & board[4] === 'X' & board[6] === 'X'){
-		alert('X GANO');
+		winnerTitle.innerText = '¡Jugador X gana!!';
 		disableButtons();
-	};
+	} else if (!board.includes(undefined)){
+		winnerTitle.innerText = 'Empate';
+		disableButtons();
+	}
 };
 
 function resetBoard(){
@@ -95,8 +102,10 @@ function resetBoard(){
 		button.innerHTML = '';
 		button.disabled = false;
 		button.style.cursor = 'pointer';
+		button.style.color = 'var(--white)'
 	});
 
+	winnerTitle.innerText = 'TIC TAC TOE';
 	flag = 0;
 	board = [ , , , , , , , , , ];
 };
